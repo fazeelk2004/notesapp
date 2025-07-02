606fa71f0b08542c97ec3390aaea0da006f4545e
Fazeel Khan <fazeelk2004@gmail.com>
2025-07-02 17:30:42 +0500

    created home page UI in frontend also installed cors in backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,10 +3,15 @@ import notesRoutes from './routes/notesRoutes.js';
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import rateLimiter from './middleware/rateLimiter.js';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
+
+app.use(cors({
+  origin: "http://localhost:5173",
+})); // Enable CORS for all routes
 
 app.use(express.json()); // Middleware To Parse JSON Bodies
 
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,8 +1,54 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { toast } from 'react-hot-toast';
+
+import Navbar from '../components/Navbar'
+import RateLimit from '../components/RateLimit'
+import NoteCard from '../components/NoteCard';
 
 const HomePage = () => {
+  const [isRateLimited, setIsRateLimited] = useState(false);
+  const [notes, setNotes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchNotes = async () => {
+      try {
+        const res = await axios.get('http://localhost:3001/api/notes');
+        console.log(res.data);
+        setNotes(res.data);
+        setIsRateLimited(false);
+      } catch (error) {
+        console.log('Error Fetching Notes:', error);
+        if (error.response?.status === 429) {
+          setIsRateLimited(true);
+        } else {
+          toast.error('Failed To Fetch Notes');
+          setIsRateLimited(false);
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchNotes();
+  },[])
+
   return (
-    <div>HomePage</div>
+    <div className='min-h-screen'>
+      <Navbar />
+      {isRateLimited && <RateLimit />}
+      <div className='mx-auto max-w-7xl p-4 mt-6'>
+        {loading && <div className='text-center text-primary py-10'>Loading Notes ...</div>}
+
+        {notes.length > 0 && !isRateLimited && (
+          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8'>
+            {notes.map(note => (
+              <NoteCard key={note._id} note={note} />
+            ))}
+          </div>   
+        )}
+      </div>
+    </div>
   )
 }
 
